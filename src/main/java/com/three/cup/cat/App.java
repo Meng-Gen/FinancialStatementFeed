8cca52a2e9e70612ac4d0fb5e29d7517106b65dd
Meng-Gen <plover@gmail.com>
2015-07-24 23:13:22 +0800

    Build feed url

--- src/main/java/com/three/cup/cat/App.java
+++ src/main/java/com/three/cup/cat/App.java
@@ -1,11 +1,13 @@
 package com.three.cup.cat;
 
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 
 public class App {
     public static void main(String[] args) {
         final FinancialStatementFeed feed = new FinancialStatementFeed();
-        feed.get("2330", "2014-12-31", true);
+        final DateTime date = new DateTime(2014, 12, 5, 0, 0);
+        feed.get("2330", date, true);
     }
 
     private static final Logger logger = Logger.getLogger(App.class);
--- src/main/java/com/three/cup/cat/FinancialStatementFeed.java
+++ src/main/java/com/three/cup/cat/FinancialStatementFeed.java
@@ -1,13 +1,30 @@
 package com.three.cup.cat;
 
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 
 public class FinancialStatementFeed {
-    public void get(String stockSymbol, String date, boolean isConsolidated) {
-        logger.info(stockSymbol);
-        logger.info(date);
-        logger.info(isConsolidated);
+    public void get(String stockSymbol, DateTime date, boolean isConsolidated) {
+        final String url = buildUrl(stockSymbol, date, isConsolidated);
+        logger.info(url);
+    }
+
+    // Url example: 
+    //     http://mops.twse.com.tw/server-java/t164sb01?step=1&CO_ID=2330&SYEAR=2014&SSEASON=4&REPORT_ID=C
+    private String buildUrl(String stockSymbol, DateTime date, boolean isConsolidated) {
+        final String year = String.valueOf(date.getYear());
+        final String season = String.valueOf(getSeason(date));
+        final String reportId = isConsolidated ? "C" : "I";
+        return String.format(urlTemplate, stockSymbol, year, season, reportId);
+    }
+
+    private int getSeason(final DateTime date) {
+        final int month = date.getMonthOfYear();
+        return (month - 1) / 3 + 1;
     }
 
     private static final Logger logger = Logger.getLogger(FinancialStatementFeed.class);
+
+    private static final String urlTemplate = "http://mops.twse.com.tw/server-java/t164sb01?" 
+            + "step=1&CO_ID=%s&SYEAR=%s&SSEASON=%s&REPORT_ID=%s";
 }
